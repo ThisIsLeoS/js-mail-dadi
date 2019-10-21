a2dcd5882b8fbbb1b1ae8926506153017b8b2543
leos <leonardo.siri.email@gmail.com>
2019-10-21 18:12:27 +0200

    delete break statement

--- mail/js/main.js
+++ mail/js/main.js
@@ -5,10 +5,8 @@ var emails, email, msg;
 emails = ["email1@gmail.com", "email2@gmail.com", "email3@gmail.com"];
 email = prompt("Enter your email");
 msg = "You can't get in!";
-for (var i = 0; i < emails.length; ++i) 
-    if (email === emails[i]) 
-    {
-        msg = "You can get in!";
-        break;
-    }
+for (var i = 0; i < emails.length; ++i)
+{
+    if (email === emails[i]) msg = "You can get in!";
+}
 document.getElementsByClassName("msg")[0].textContent = msg;
